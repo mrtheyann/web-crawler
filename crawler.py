b14d1481d3a4cd32ff6a541ab623dd20c961d42d
mrtheyann <darklodr@gmail.com>
2017-04-06 02:04:20 +0500

    single thread parsing implemented

--- crawler.py
+++ crawler.py
@@ -1,6 +1,9 @@
 import requests
+import csv
 
 from bs4 import BeautifulSoup 
+
+from datetime import datetime
 
 def get_html(url):
 	response = requests.get(url)
@@ -20,14 +23,56 @@ def get_all_links(html):
 
 	return links 
 
+def get_page_data(html):
+	soup = BeautifulSoup(html, 'lxml')
+
+	try:
+		name = soup.find('h1', class_='text-large').text.strip()
+
+	except:
+		name = ''
+
+	try:
+		price = soup.find('span', id='quote_price').text.strip()
+	except:
+		price = ''
+
+	data = {'name' : name,
+					'price' : price}
+
+	return data
+
+def write_csv(data):
+	with open('coinmarketcap.csv', 'a') as f:
+		writer = csv.writer(f)
+
+		writer.writerow( (data['name'],
+											data['price']) )
+		print(data['name'], 'parsed')
+
+
+
 def main():
+	
+	#---------------------
+	start = datetime.now()
+	#---------------------
 	url = 'https://coinmarketcap.com/all/views/all'
 
-
 	all_links = get_all_links( get_html(url) )
-	
-	for i in all_links:
-		print(i)
+
+	for index, url in enumerate(all_links):
+		html = get_html(url)
+		data = get_page_data(html)
+		write_csv(data)
+		print(index)
+
+	#---------------------
+	end = datetime.now()
+
+	total = end - start
+	#---------------------
+	print(str(total))
 
 if __name__ == '__main__':
 	main()
